55c0fc72f5c5f1d931bc12463af293dca2b26b7b
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-12-19 15:47:19 -0800

    Update index2.sh

--- index2.sh
+++ index2.sh
@@ -2,13 +2,13 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="0; url=http://nasihosting.com/cgi-bin/index2.sh">
+        <meta http-equiv="refresh" content="0; url=http://nasihosting.com/cgi-bin/index-httponly.sh">
         <script type="text/javascript">
-            window.location.href = "http://nasihosting.com/cgi-bin/index2.sh"
+            window.location.href = "http://nasihosting.com/cgi-bin/index-httponly.sh"
         </script>
         <title>Daftar nasihosting.com</title>
     </head>
     <body>
-              Jika tidak redirect otomatis, maka klik <a href='http://nasihosting.com/cgi-bin/index2.sh'>Link ke daftar nasihosting.com</a>.
+              Jika tidak redirect otomatis, maka klik <a href='http://nasihosting.com/cgi-bin/index-httponly.sh'>Link ke daftar nasihosting.com</a>.
     </body>
 </html>
