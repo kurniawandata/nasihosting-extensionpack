16220ee45d7b795a54dba718d0cf9c27ee735885
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-12-19 14:17:32 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+echo "Instalasi Nasihosting-Extensionpack   "
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+sudo a2enmod cgi
+sudo service apache2 restart
+sudo index.html /var/www/html
+sudo cp index.sh /usr/lib/cgi-bin
+sudo cp run.sh /usr/lib/cgi-bin
+sudo chmod 777 /usr/lib/cgi-bin
+sudo chmod 777 /usr/lib/cgi-bin/*
+sudo chmod 777 /home
+sudo chmod 777 /etc/apache2/sites-available
+sudo nano /usr/lib/cgi-bin/run.sh
+sudo sed -i "/more/i\www-data ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
+sudo nano /etc/sudoers, isi : 
+sudo a2enmod ssl
+sudo mkdir /etc/apache2/ssl
+sudo chmod 777 /etc/apache2/ssl
+sudo service apache2 restart
