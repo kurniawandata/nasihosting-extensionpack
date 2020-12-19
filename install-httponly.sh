c26dff480d5234ddb6f0c91a1c587dd8e5f05983
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-12-19 14:34:16 -0800

    Create install-httponly.sh

---
+++ install-httponly.sh
@@ -0,0 +1,25 @@
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+echo "Instalasi Nasihosting-Extensionpack   "
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+echo -n "Masukkan nama domain : "
+read domain
+sed -i "s/nasihosting.com/$domain/g" index-httponly.html
+sed -i "s/nasihosting.com/$domain/g" index-httponly.sh
+sudo mkdir /home/root
+sudo touch /home/root/locked
+sudo a2enmod cgi
+sudo service apache2 restart
+sudo index.html /var/www/html
+sudo cp index.sh /usr/lib/cgi-bin
+sudo cp run.sh /usr/lib/cgi-bin
+sudo chmod 777 /usr/lib/cgi-bin
+sudo chmod 777 /usr/lib/cgi-bin/*
+sudo chmod 777 /home
+sudo chmod 777 /etc/apache2/sites-available
+sudo nano /usr/lib/cgi-bin/run.sh
+sudo sed -i "/more/i\www-data ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
+sudo nano /etc/sudoers, isi : 
+sudo a2enmod ssl
+sudo mkdir /etc/apache2/ssl
+sudo chmod 777 /etc/apache2/ssl
+sudo service apache2 restart
