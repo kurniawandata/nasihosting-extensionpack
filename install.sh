a36d410e368f42e1b636a6a0b3e3f223da866f5b
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-12-19 16:56:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@ echo "Instalasi Nasihosting-Extensionpack - httponly  "
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
 echo -n "Masukkan nama domain : "
 read domain
+echo -n "Masukkan password root MySQL : "
+read passwordmysql
+passwordmysql="-p$passwordmysql"
+sed -i "s/-ppasswordmysql/$passwordmysql/g" run-httponly.sh
 sed -i "s/nasihosting.com/$domain/g" index.html
 sed -i "s/nasihosting.com/$domain/g" index.sh
 sed -i "s/nasihosting.com/$domain/g" run.sh
 sed -i "s/nasihosting.com/$domain/g" support/subdomain.conf
+sed -i "s/-ppasswordmysql/$passwordmysql/g" run.sh
 sudo nano support/subdomain.conf
 sudo mkdir /home/root
 sudo touch /home/root/locked
