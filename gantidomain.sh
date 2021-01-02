1efd7745b4c05a67185ac74cb648de0a09e0f174
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-01-02 15:00:49 -0800

    Create gantidomain.sh

---
+++ gantidomain.sh
@@ -0,0 +1,9 @@
+echo -n "Masukkan nama domain lama : "
+read domainlama
+echo -n "Masukkan nama domain baru : "
+read domainbaru
+sed -i "s/$domainlama/$domainbaru/g" index.html
+sed -i "s/$domainlama/$domainbaru/g" index.sh
+sed -i "s/$domainlama/$domainbaru/g" run.sh
+sed -i "s/$domainlama/$domainbaru/g" support/subdomain.conf
+service apache2 restart
