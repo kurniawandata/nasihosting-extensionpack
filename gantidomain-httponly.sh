375c04fc539257e7497b276c161247875a54d3bc
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-01-02 15:34:36 -0800

    Update gantidomain-httponly.sh

--- gantidomain-httponly.sh
+++ gantidomain-httponly.sh
@@ -2,7 +2,8 @@ echo -n "Masukkan nama domain lama : "
 read domainlama
 echo -n "Masukkan nama domain baru : "
 read domainbaru
-sed -i "s/$domainlama/$domainbaru/g" index.html
+sed -i "s/$domainlama/$domainbaru/g" index2.html
+sed -i "s/$domainlama/$domainbaru/g" /var/www/html/index.html
 sed -i "s/$domainlama/$domainbaru/g" index-httponly.sh
 sed -i "s/$domainlama/$domainbaru/g" run-httponly.sh
 sed -i "s/$domainlama/$domainbaru/g" support/subdomain2.conf
