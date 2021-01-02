c270574683c943903699a69e1bbc92c307efe7a0
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-01-02 15:35:14 -0800

    Update gantidomain.sh

--- gantidomain.sh
+++ gantidomain.sh
@@ -3,6 +3,7 @@ read domainlama
 echo -n "Masukkan nama domain baru : "
 read domainbaru
 sed -i "s/$domainlama/$domainbaru/g" index.html
+sed -i "s/$domainlama/$domainbaru/g" /var/www/html/index.html
 sed -i "s/$domainlama/$domainbaru/g" index.sh
 sed -i "s/$domainlama/$domainbaru/g" run.sh
 sed -i "s/$domainlama/$domainbaru/g" support/subdomain.conf
