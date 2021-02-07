fa2b30db5e9bd395fb2f150f5510be353e403589
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-02-07 01:07:05 -0800

    Update gantidomain.sh

--- gantidomain.sh
+++ gantidomain.sh
@@ -6,8 +6,8 @@ sed -i "s/$domainlama/$domainbaru/g" index.html
 sed -i "s/$domainlama/$domainbaru/g" /var/www/html/index.html
 sed -i "s/$domainlama/$domainbaru/g" /usr/lib/cgi-bin/index.sh
 sed -i "s/$domainlama/$domainbaru/g" /usr/lib/cgi-bin/run.sh
+sed -i "s/$domainlama/$domainbaru/g" /usr/lib/cgi-bin/aktivasi3.sh
 sed -i "s/$domainlama/$domainbaru/g" index.sh
 sed -i "s/$domainlama/$domainbaru/g" run.sh
-sed -i "s/$domainlama/$domainbaru/g" aktivasi3.sh
 sed -i "s/$domainlama/$domainbaru/g" support/subdomain.conf
 service apache2 restart
