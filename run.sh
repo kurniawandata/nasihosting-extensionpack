c3b1a07cd153139cdd89d5088b2f804ce26a5b97
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-02-07 00:22:24 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@ echo "Content-type: text/html"
 echo ""
 name=`echo "$QUERY_STRING" | awk '{split($0,array,"&")} END{print array[1]}' | awk '{split($0,array,"=")} END{print array[2]}'`
 password=`echo "$QUERY_STRING" | awk '{split($0,array,"&")} END{print array[2]}' | awk '{split($0,array,"=")} END{print array[2]}'`
+wa=`echo "$QUERY_STRING" | awk '{split($0,array,"&")} END{print array[3]}' | awk '{split($0,array,"=")} END{print array[2]}'`
 if [ -z "$(ls -A /home/$name)" ]; then
+echo $name, $password, $wa. > /home/datauser/$name
 sudo mkdir /home/$name
 sudo cp /home/nasihosting-extensionpack/filemanager/* /home/$name
 sudo chmod 777 /home/$name
@@ -13,9 +15,13 @@ cp /home/nasihosting-extensionpack/support/subdomain.conf /etc/apache2/sites-ava
 sed -i "s/sample/$name/g" /etc/apache2/sites-available/$name.conf
 sudo a2ensite $name.conf
 sudo systemctl reload apache2
+sudo cp /usr/lib/cgi-bin/aktivasi3.sh aktivasi4.sh
+sed -i "s/unik/$name/g" /usr/lib/cgi-bin/aktivasi4.sh
+chmod 777 aktivasi4.sh
+./aktivasi4.sh
+rm aktivasi4.sh
 mysql -uroot -ppasswordmysql -e "CREATE DATABASE "$name""
 mysql -uroot -ppasswordmysql -e "GRANT ALL PRIVILEGES ON "$name".* TO "$name"@localhost IDENTIFIED BY '"$password"'"
-sudo clear
 cat <<EOT
 <!DOCTYPE html>
 <html>
@@ -35,7 +41,9 @@ Login : https://$name.nasihosting.com/phpmyadmin<br />
 Username : $name <br />
 Password : $password <br />
 <br />
-Saat ini request anda tinggal diapprove oleh admin<br />
+Saat ini subdomain anda sudah aktif
+<br />
+File login.php dan config.php tidak boleh dihapus. 
 </body>
 </html>
 EOT
