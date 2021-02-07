1b845437013f5bd6b4d628c8b7b4d276b3cbc86f
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-02-07 00:42:40 -0800

    Update index-httponly.sh

--- index-httponly.sh
+++ index-httponly.sh
@@ -9,7 +9,7 @@ cat <<EOT
 <head>
     <title>Pendaftaran</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <style>
+ <style>
     .captcha
     { 
         font: bold 16px;
